b7500a0e0f4b547ab7aedbe0bc7e16eca8e2fa44
KoksTheSupa <kogome009@yandex.ru>
2023-01-12 01:54:08 +0500

    feat: added new graph

--- main/solutionDECIMAL.py
+++ main/solutionDECIMAL.py
@@ -5,14 +5,14 @@ from random import randint
 
 import matplotlib.pyplot as plt
 
-N = 101
+N = 100
 DJ = Decimal("0.16")
-OMEGA = Decimal("-0.32")
+OMEGA = Decimal("-0.28")
 B = Decimal("0.15")
 EPS = H = Decimal(str(10**-6))
-S = [0] * (N + 1)  # np.zeros(N + 1)
+S = [0] * (N+1)  # np.zeros(N + 1)
 DELIMITER = 2000000000  # double results[2000000000]
-s0 = Decimal("0.156407196400000")
+s0 = Decimal("0.495373257505000")
 temp = Decimal(Decimal("0.4") / (DELIMITER))
 
 
@@ -93,6 +93,9 @@ def main():
     print(f'Omega = {OMEGA}, eps = {EPS}\n')
     plot(N + 1, counter(s0, N))
     writef(S)
+    plt.plot([-0.32, -0.28, -0.22, -0.2, -0.18], [0.165036570213893, 0.509457626013908,
+                                                  0.736884391556128, 0.788864920607633, 0.833085737950537])
+    plt.show()
 
 
 if __name__ == "__main__":
